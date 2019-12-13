6c21916d0277b9d4316d75cbaa6dc3e5327600ea
kitsune-exe <yuh027@gmail.com>
2019-12-13 17:18:02 +0800

    parameter change

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -20,7 +20,7 @@ starttime = time.time()
 clf4 = MLPClassifier(activation='relu',
                      solver='sgd',
                      max_iter=1000,
-                     hidden_layer_sizes=(100,),#best: 50, 50?
+                     hidden_layer_sizes=(25, 10),#best: 50, 50?
                      random_state=None)
 clf4 = clf4.fit(train_x, train_y)
 
